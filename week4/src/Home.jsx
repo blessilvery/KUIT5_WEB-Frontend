e9bb733bf53e45c2314dfb790b2750af0ca2853e
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-04-04 23:40:50 +0900

    feat: week4 setting

---
+++ week4/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {};
+
+const Content = () => {};
+
+const BottomNav = () => {};
+
+const Home = () => {
+  return <div>Homedd</div>;
+};
+
+export default Home;
