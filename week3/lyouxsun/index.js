9e5673c859779ec76ed7133b20ca56ba19996b2e
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-04-04 15:45:44 +0900

    feat: 기존 todo content 수정 기능 구현

--- week3/lyouxsun/index.js
+++ week3/lyouxsun/index.js
@@ -11,9 +11,10 @@ form.addEventListener("submit", (e) => {
     const newTodo = {
       id: Date.now(),
       text,
-      done: false
+      done: false,
     };
-    todos.push(newTodo);
+    // todos.push(newTodo);
+    todos = [...todos, newTodo];
     document.getElementById("todo-input").value = "";
     render();
   }
@@ -21,37 +22,57 @@ form.addEventListener("submit", (e) => {
 
 function deleteTodo(id) {
   // 배열을 순회하며 id가 동일한 todo를 찾는다. -> 해당 요소를 제거하기
-  
-  const deleteTodo = todos.filter(todo => {
-    todo.id == id;
-  });
-  console.log(deleteTodo.id);
-  console.log(deleteTodo.text);
-  todos.delete(deleteTodo);
+  const deleteTodo = todos.find((todo) => todo.id == id);
+  const deleteIdx = todos.indexOf(deleteTodo);
+  todos.splice(deleteIdx, 1);
+  render();
 }
 
 function toggleDone(id) {
-  
+  const doneTodo = todos.find((todo) => todo.id == id);
+  if (doneTodo.done == true) {
+    doneTodo.done = false;
+  } else {
+    doneTodo.done = true;
+  }
+  render();
 }
 
 function render() {
   list.innerHTML = "";
-  todos.forEach(todo => {
+  todos.forEach((todo) => {
     const li = document.createElement("li");
     li.className = todo.done ? "done" : "";
-    
+
     const span = document.createElement("span");
     span.textContent = todo.text;
     span.style.cursor = "pointer";
     span.onclick = () => toggleDone(todo.id);
-    
+
     const delBtn = document.createElement("button");
+    delBtn.getElementsByClassName.value = "update";
     delBtn.textContent = "삭제";
+    delBtn.className = "delete";
     delBtn.onclick = () => deleteTodo(todo.id);
 
+    const updBtn = document.createElement("button");
+    updBtn.textContent = "수정";
+    updBtn.className = "update";
+    updBtn.onclick = () => updateTodo(todo.id);
+
+    const actions = document.createElement("div");
+    actions.className = "actions";
+    actions.appendChild(delBtn);
+    actions.appendChild(updBtn);
+
     li.appendChild(span);
-    li.appendChild(delBtn);
+    li.appendChild(actions);
     list.appendChild(li);
   });
 }
-function updateTodo(){}
+function updateTodo() {
+  const doneTodo = todos.find((todo) => todo.id == id);
+  doneTodo.setElement = "input";
+  
+  render();
+}
