694be9433a9487f5fbd948920f021125e974da47
Jung-Eon <egg9054@gmail.com>
2025-04-06 00:25:21 +0900

    step1 complete

---
+++ week4/jung-eon/mission/src/models/MarketModels.js
@@ -0,0 +1,57 @@
+const marketModel = {
+  location: "군자동",
+  items: [
+    {
+      title: "에어팟 프로",
+      location: "군자동",
+      timeAgo: "3일 전",
+      price: "220,000원",
+      image: "/assets/post__img1.svg",
+      comments: 3,
+      likes: 11,
+      isSold: true,
+    },
+    {
+      title: "바이레도 블랑쉬 50ml",
+      location: "광진구 구의제3동",
+      timeAgo: "26초 전",
+      price: "4,000원",
+      image: "/assets/post__img2.svg",
+      comments: 0,
+      likes: 2,
+      isSold: false,
+    },
+    {
+      title: "샌드위치",
+      location: "동대문구 휘경동",
+      timeAgo: "끝물 59초 전",
+      price: "8,000원",
+      image: "/assets/post__img3.svg",
+      comments: 0,
+      likes: 0,
+      isSold: false,
+    },
+    {
+      title: "아이폰 13프로맥스",
+      location: "군자동",
+      timeAgo: "1일 전",
+      price: "1,000,000원",
+      image: "/assets/post__img4.svg",
+      comments: 0,
+      likes: 0,
+      isSold: true,
+    },
+    {
+      title: "커피머신",
+      location: "구리시 교문1동",
+      timeAgo: "1초 전",
+      price: "100,000원",
+      image: "/assets/post__img5.svg",
+      comments: 0,
+      likes: 0,
+      isSold: true,
+    },
+  ],
+};
+
+export default marketModel;
