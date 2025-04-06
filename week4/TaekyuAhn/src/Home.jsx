afacd8196b43af92ef6eed7d69aa545fb70b9837
Chris Ahn <chrisahn1007@gmail.com>
2025-04-06 17:27:42 +0900

    MarketModel파일의 데이터 객체와 Item컴포넌트 매핑

--- week4/TaekyuAhn/src/Home.jsx
+++ week4/TaekyuAhn/src/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./index.module.scss";
 import Item from "./components/Item";
+import marketModel from "./models/MarketModel";
 
 const Header = () => {
+  const { location } = marketModel;
   return (
     <div className={styles.headerSection}>
       <div className={styles.locationBox}>
-        <div className={styles.locationBox__text}>군자동</div>
+        <div className={styles.locationBox__text}>{location}</div>
         <img src="./imgs/down-chevron.svg" alt="" />
       </div>
       <div className={styles.utilBox}>
@@ -19,9 +21,10 @@ const Header = () => {
 };
 
 const Content = () => {
+  const { items } = marketModel;
   return (
     <div className={styles.contentSection}>
-      <Item></Item>
+      {items.map((item, index) => (item.isSold ? "" : <Item key={index} {...item}></Item>))}
     </div>
   );
 };
--- week4/TaekyuAhn/src/components/Item.jsx
+++ week4/TaekyuAhn/src/components/Item.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import styles from "../index.module.scss";
 
-function Item() {
+function Item({ title, location, timeAgo, price, image, comments, likes, isSold }) {
   return (
     <div className={styles.itemBox}>
       <div className={styles.imageArea}>
-        <img className={styles.imageArea__image} src="./imgs/image1.png" alt="" />
+        <img className={styles.imageArea__image} src={image} alt="" />
       </div>
       <div className={styles.contentArea}>
         <div className={styles.contentArea__info}>
-          <div className={styles.contentArea__info__title}>에어팟프로</div>
-          <div className={styles.contentArea__info__location}>군자동 &#183; 3일전</div>
-          <div className={styles.contentArea__info__price}>100원</div>
+          <div className={styles.contentArea__info__title}>{title}</div>
+          <div className={styles.contentArea__info__location}>
+            {location} &#183; {timeAgo}
+          </div>
+          <div className={styles.contentArea__info__price}>{price}</div>
         </div>
         <div className={styles.contentArea__reaction}>
           <div className={styles.rowSet}>
             <img src="./imgs/comments.svg" alt="" />
-            <div>3</div>
+            <div>{comments}</div>
           </div>
           <div className={styles.rowSet}>
             <img src="./imgs/heart.svg" alt="" />
-            <div>11</div>
+            <div>{likes}</div>
           </div>
         </div>
       </div>
