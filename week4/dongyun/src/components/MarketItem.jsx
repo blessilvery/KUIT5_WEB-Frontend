906755c2bd9a326821f23f24e15fea8ade16b3f7
Glyph8 <newdy0808@gmail.com>
2025-04-09 15:51:11 +0900

    refactor: 비구조화 할당 적용 & 이미지 절대경로 적용

--- week4/dongyun/src/components/MarketItem.jsx
+++ week4/dongyun/src/components/MarketItem.jsx
@@ -1,16 +1,17 @@
 
 function MarketItem({ item }) {
+    const { title, location, timeAgo, price, image, comments, likes } = item
     return (
         <article className="market-item">
             <div className="item-img">
-                <img src={`../../public/dummy_assets/${item.image}`} />
-                {/* <img src={require(`../../public/dummy_assets/${item.image}`).default} /> */}
+                {/* <img src={`../../public/dummy_assets/${image}`} /> */}
+                <img src={`/dummy_assets/${image}`} />
             </div>
             <div className="item-info">
                 <div className="item-description">
-                    <p className="item-name">{item.title}</p>
-                    <p className="item-loc-date">{item.location} · {item.timeAgo}</p>
-                    <p className="item-price">{item.price}</p>
+                    <p className="item-name">{title}</p>
+                    <p className="item-loc-date">{location} · {timeAgo}</p>
+                    <p className="item-price">{price}</p>
                 </div>
                 {/* <!-- <p>채팅 이모지 / 채팅 개수 / 하트 이모지 / 하트 개수 (모두 0이라면 없음)</p> --> */}
                 <div className="offer-info">
@@ -19,11 +20,11 @@ function MarketItem({ item }) {
                             className="offer-info-img"
                             src="./assets/offer_chat_count_icon.svg"
                         />
-                        <p className="offer-info-detail">{item.comment}</p>
+                        <p className="offer-info-detail">{comments}</p>
                     </span>
                     <span className="offer-info-box">
                         <img className="offer-info-img" src="./assets/heart_icon.svg" />
-                        <p className="offer-info-detail">{item.likes}</p>
+                        <p className="offer-info-detail">{likes}</p>
                     </span>
                 </div>
             </div>
