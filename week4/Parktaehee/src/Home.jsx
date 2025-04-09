b1932d85493ee8c79dd02eda22f6df33d301b4f4
qkrxogmla <suk05553@naver.com>
2025-04-10 04:39:23 +0900

    step3 complete

--- week4/Parktaehee/src/Home.jsx
+++ week4/Parktaehee/src/Home.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import BottomNav from "./components/BottomNav";
 
+//marketModel을 한 번 import해서 그 안의 items를 비구조화 할당으로 한 번에 꺼내 사용
 const { items } = marketModel;
 
 const Home = () => {
