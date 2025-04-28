499578a11c6b404bed31b717746f2e7ca35b4645
jean <super1817@naver.com>
2025-04-28 20:12:38 +0900

    example

--- week5/blessilvery/src/App.js
+++ week5/blessilvery/src/App.js
@@ -1,8 +1,18 @@
-function App() {
+import FilterableProductTable from "./FilterableProductTable";
+
+const PRODUCTS = [
+  {category:"Fruits",price: "$1",stocked: true,name: "Apple"},
+  {category:"Fruits",price: "$1",stocked: true,name: "Dragonfruit"},
+  {category:"Fruits",price: "$2",stocked: false,name: "Passionfruit"},
+  {category:"Vegetables",price: "$2",stocked: true,name: "Spinach"},
+  {category:"Vegetables",price: "$4",stocked: false,name: "Pumpkin"},
+  {category:"Vegetables",price: "$1",stocked: true,name: "Peas"},
+]
+
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <FilterableProductTable products = {PRODUCTS}/>
   );
-}
+};
 
 export default App;
