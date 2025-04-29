a5bf369c8528d7ab16c6bdbca9eb785d5dfabdd5
TakioN <jungbeen0724@gmail.com>
2025-04-30 03:34:08 +0900

    feat: implement thinking in react

--- week5/jungbeen/src/App.js
+++ week5/jungbeen/src/App.js
@@ -1,5 +1,16 @@
+import FilterableProductTable from "./FilterableProductTable";
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
 const App = () => {
-  return <div className="App">Hello World</div>;
+  return <FilterableProductTable products={PRODUCTS} />;
 };
 
 export default App;
