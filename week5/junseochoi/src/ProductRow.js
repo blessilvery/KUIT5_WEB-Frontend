3abd97f30709681bfede9fc0c4441748bb584e35
[evan7484] <[evan7484@gmail.com]>
2025-05-02 14:29:49 +0900

    수정까지 구현 완료

--- week5/junseochoi/src/ProductRow.js
+++ week5/junseochoi/src/ProductRow.js
@@ -3,23 +3,58 @@ import DeleteProductRow from "./DeleteProductRow";
 import ModifyProductRow from "./ModidyProductRow";
 
 const ProductRow = ({ product, setProducts }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedName, setEditedName] = useState(product.name);
+  const [editedPrice, setEditedPrice] = useState(product.price);
+
   const deleteProduct = (productToDelete) => {
     setProducts((prevProducts) =>
       prevProducts.filter((p) => p.name !== productToDelete.name)
     );
   };
 
+  const productToModify = () => {
+    setProducts((prev) =>
+      prev.map((p) =>
+        p.name === product.name
+          ? { ...p, name: editedName, price: editedPrice }
+          : p
+      )
+    );
+    setIsEditing(false);
+  };
+
   return (
     <tr>
       <td style={{ color: product.stocked ? "black" : "red" }}>
-        {product.name}
+        {isEditing ? (
+          <input
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+          />
+        ) : (
+          product.name
+        )}
       </td>
-      <td>{product.price}</td>
+      <td>
+        {isEditing ? (
+          <input
+            value={editedPrice}
+            onChange={(e) => setEditedPrice(e.target.value)}
+          />
+        ) : (
+          product.price
+        )}
+      </td>
       <td>
         <DeleteProductRow product={product} deleteProduct={deleteProduct} />
       </td>
       <td>
-        <ModifyProductRow />
+        {isEditing ? (
+          <button onClick={productToModify}>저장</button>
+        ) : (
+          <button onClick={() => setIsEditing(true)}>수정</button>
+        )}
       </td>
     </tr>
   );
