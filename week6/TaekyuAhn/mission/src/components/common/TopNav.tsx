133752fcff8bbb2af99b6fc2d85f081f091d3a66
Chris Ahn <chrisahn1007@gmail.com>
2025-05-06 03:08:52 +0900

    feat / Cart 컴포넌트 구현

--- week6/TaekyuAhn/mission/src/components/common/TopNav.tsx
+++ week6/TaekyuAhn/mission/src/components/common/TopNav.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./TopNav.module.scss";
 
-const TopNav = () => {
+type Props = {
+  text?: string;
+  clickHandler?: () => void;
+};
+
+const TopNav = ({ text, clickHandler }: Props) => {
   return (
     <div className={styles.navBar}>
       <img src="/imgs/left-chevron.svg" alt="왼쪽 화살표 이미지" />
+      <div onClick={clickHandler} className={styles.navBar__text}>
+        {text}
+      </div>
     </div>
   );
 };
