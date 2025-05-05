68c8bc3d886df7f6edaf1839d16d37ceacd1f266
kimdohyeon <kimdohyeon2732@konkuk.ac.kr>
2025-05-06 00:31:51 +0900

    [Feat] routing,styling 실습

---
+++ week6/dohyeonkk/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Router from './routes/Router'
+import GlobalStyles from "./components/GlobalStyles";
+
+const App = () => {
+
+  return(<>
+    <GlobalStyles/>
+    <Router/>
+    </>
+  )
+
+};
+
+export default App;
---
+++ week6/dohyeonkk/src/components/SearchBar.Style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const SearchForm = styled.form`
+display : flex;
+justify-content : space-between;
+background-color : #ededed;
+padding : 10px 20px;
+border-bottom : 1px solid #ededed;
+`;
