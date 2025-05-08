4a35dc661921cf7e5827c3de4544c316b8dab3b3
[evan7484] <[evan7484@gmail.com]>
2025-05-08 23:17:36 +0900

    페이지 전부 구현 완료

---
+++ week6/junseochoi/src/models/OrderContentList.jsx
@@ -0,0 +1,11 @@
+const OrderContentList = [
+  {
+    StoreName: "샐로리 한남점",
+    MenuName: "토마토 샐러드",
+    Material: "추천소스, 채소볼, 베이컨추가, 시저드레싱추가",
+    Price: "10,600원",
+    Count: "1개",
+  },
+];
+
+export default OrderContentList;
