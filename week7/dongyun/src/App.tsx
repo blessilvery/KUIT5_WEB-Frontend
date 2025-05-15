7e8d49da7f8d5bcd094c42d113c214b1715e6edc
glyph8 <newdy0808@gmail.com>
2025-05-15 14:38:04 +0900

    선택과제 구현, 연속 뒤집기 디버깅

--- week7/dongyun/src/App.tsx
+++ week7/dongyun/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     const maxScore = 5;
     const [score, setScore] = useState(0);
     const [numArray, setNumArray] = useState(Array.from({length: cardCount}, (_, i) => i));
+    const [leftTime, setLeftTime] = useState(60);
     const {cardArray, setCardArray} = useGame(cardCount)
 
     const addScore = (): void => {
@@ -26,17 +27,41 @@ function App() {
         return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // 최댓값도 포함, 최솟값도 포함
     }
 
-    const handleResetBtn = () => {
+    const resetGame = () =>{
         const newArray = [...numArray];
         newArray.sort(() => getRandomIntInclusive(-1, 1))
         setNumArray(newArray);
         setCardArray(createCardPair(cardCount));
         setScore(0);
+        setLeftTime(60)
+    }
+
+    const handleResetBtn = () => {
+        resetGame()
+        const intervalId = setInterval(()=>{
+            setLeftTime(leftTime=> {
+                if(leftTime < 1){
+                    clearInterval(intervalId)
+                    const wait5s = new Promise(resolve => {
+                        setTimeout(resolve, 5000)
+                    })
+                    wait5s.then(() => {
+                        resetGame()
+                    }, () => {
+                        alert('제한시간 로직 문제 발생')
+                    })
+                }
+                return leftTime - 1 ;
+            })
+            }, 1000
+        )
+
     }
 
     return (
         <S.Container>
             <S.NavBar><h3>맞힌 개수 : {score}/{maxScore}</h3></S.NavBar>
+            <S.NavBar>{leftTime > 0 ? <h5>남은 시간 : {leftTime}</h5> : "실패하셨습니다. 5초 뒤, 게임이 리셋됩니다."}</S.NavBar>
             {score === maxScore ? <S.Reward><h2>정답입니다!</h2></S.Reward> : null}
             <S.CardGame>
                 {
--- week7/dongyun/src/hooks/useGame.ts
+++ week7/dongyun/src/hooks/useGame.ts
@@ -1,41 +1,40 @@
 import {useEffect, useState} from "react";
 import type {CardArray} from "../types/card.ts";
 
-export const createCardPair = (cardCount:number):CardArray[] => {
+export const createCardPair = (cardCount: number): CardArray[] => {
     return Array.from({length: cardCount}, (_, i): CardArray => ({
         index: i, isOpened: false, isCorrect: false
     }))
 }
 
-const useGame = (cardCount:number) => {
+const useGame = (cardCount: number) => {
     const [cardArray, setCardArray] = useState(createCardPair(cardCount));
 
     useEffect(() => {
         const mismatchOpened = cardArray.filter((card) => card.isOpened)
-        if (mismatchOpened.length === 2) {
+        if (mismatchOpened.length > 1) {
             console.log(mismatchOpened)
-            const [misCard1, misCard2] = mismatchOpened;
-            const newCardArray = cardArray.map((card) => {
-                if (card.index === misCard1.index || card.index === misCard2.index) {
-                    return ({...card, isOpened: false});
-                } else
-                    return ({...card})
-            })
             const waitSeconds = new Promise(resolve => {
                 setTimeout(resolve, 3000)
             })
-            console.log("짝 안맞는 카드 원상 복구")
-            console.log(cardArray)
             waitSeconds.then(() => {
+                const newCardArray = cardArray.map((card) => {
+                    if (mismatchOpened.map((misCard) => {
+                        return card.index === misCard.index;
+                    }))
+                        return ({...card, isOpened: false});
+                    else
+                        return ({...card})
+                })
                 setCardArray(newCardArray)
+                console.log("3초후", cardArray)
             }, () => {
                 alert('대기 로직 문제 발생')
             })
-            console.log(cardArray)
         }
     }, [cardArray])
 
-    return { cardArray , setCardArray}
+    return {cardArray, setCardArray}
 };
 
 export default useGame;
